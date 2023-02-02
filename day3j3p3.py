44cc33a382f83a69f6c40d277fa6adc1e0558c17
20A31A05J3 <109794027+20A31A05J3@users.noreply.github.com>
2023-02-02 15:32:06 +0530

    Delete day3j3p3.py

--- day3j3p3.py
+++
@@ -1,9 +0,0 @@
-size=int(input("enter the size of list:"))
-L=[]
-for i in range(size):
-    element=int(input("enter an element:"))
-    L.append(element)
-print(L)
-for j in L:
-    if j%2==0:
-        print(j)
